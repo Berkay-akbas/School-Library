69eede363b9b3193f25f6e04b1b702dfcf44116d
Berkay Akbas <berkayakbas@gmail.com>
2022-09-28 23:32:08 +0300

    Add decorators

--- person.rb
+++ person.rb
@@ -1,6 +1,10 @@
-class Person
-  def initialize(age, name: 'unknown', parent_permisison: true)
-    @id = 0
+require './nameable'
+require './decorator'
+
+
+class Person < Nameable
+  def initialize(age, name = 'unknown', parent_permisison: true)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permisison = parent_permisison
@@ -10,7 +14,11 @@ class Person
   attr_writer :name, :age
 
   def can_use_services?
-    @parent_permisison || @age >= 18
+    @parent_permisison || of_age?
+  end
+
+  def correct_name
+    @name
   end
 
   private
@@ -19,3 +27,11 @@ class Person
     @age >= 18
   end
 end
+
+
+person = Person.new(22, 'maximilianus')
+  person.correct_name
+  capitalizedPerson = CapitalizeDecorator.new(person)
+ puts capitalizedPerson.correct_name
+  capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+  puts capitalizedTrimmedPerson.correct_name
